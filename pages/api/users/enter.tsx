f0c9a46d334272a30b1312d6ed17bf35f4549a5c
김덕현 <oraclian@estsecurity.com>
2022-02-23 08:37:01 +0900

    Accounts Login

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@libs/server/withHandler';
+import client from '@libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+
+  let user = await client.user.upsert({
+    where: {
+      ...(phone && { phone: +phone }),
+      ...(email && { email }),
+    },
+    create: {
+      name: 'Anonymous',
+      ...(phone && { phone: +phone }),
+      ...(email && { email }),
+    },
+    update: {},
+  });
+  console.log(user);
+
   res.status(200).json({ ok: true });
 }
 
