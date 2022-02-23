8cf74748c338edbc71bc188dfd4c6d0d6f684762
김덕현 <oraclian@estsecurity.com>
2022-02-23 16:57:45 +0900

    me bug fix

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -4,11 +4,10 @@ import client from '@libs/server/client';
 import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  console.log(req.session.user);
+  if (!req.session.user) return res.status(401).json({ ok: false, message: 'Unauthorized' });
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
-  if (!profile) return res.status(404).end();
   res.json({ ok: true, profile });
 }
 
