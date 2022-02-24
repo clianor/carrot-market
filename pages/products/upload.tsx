85dccaa1a6ed890a0be3bce41c9ed2d2bf6a26d9
김덕현 <oraclian@estsecurity.com>
2022-02-24 11:43:24 +0900

    Upload API

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -3,6 +3,9 @@ import Input from '@components/input';
 import Layout from '@components/layout';
 import TextArea from '@components/textarea';
 import useMutation from '@libs/client/useMutation';
+import { Product } from '@prisma/client';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { NextPageWithLayout } from '../_app';
 
@@ -12,14 +15,26 @@ interface UploadProductForm {
   description: string;
 }
 
+interface UploadProductMutation {
+  ok: boolean;
+  product: Product;
+}
+
 const Upload: NextPageWithLayout = () => {
+  const router = useRouter();
   const { register, handleSubmit } = useForm<UploadProductForm>();
-  const [uploadProduct, { loading, data }] = useMutation('/api/products');
+  const [uploadProduct, { loading, data }] = useMutation<UploadProductMutation>('/api/products');
 
   const onValid = (data: UploadProductForm) => {
     if (loading) return;
     uploadProduct(data);
   };
+
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/products/${data.product.id}`);
+    }
+  }, [data, router]);
 
   return (
     <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
@@ -53,7 +68,7 @@ const Upload: NextPageWithLayout = () => {
         register={register('price', { required: true })}
         required
         label="Price"
-        placeholder="0.00"
+        placeholder="0"
         name="price"
         type="text"
         kind="price"
