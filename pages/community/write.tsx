7bfa850adc53a30ce6ab085e98dc25f704ada32c
김덕현 <oraclian@estsecurity.com>
2022-02-24 16:52:10 +0900

    Forms and Handlers

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,13 +1,46 @@
-import TextArea from '@components/textarea';
 import Button from '@components/button';
-import { NextPageWithLayout } from '../_app';
 import Layout from '@components/layout';
+import TextArea from '@components/textarea';
+import useMutation from '@libs/client/useMutation';
+import { Post } from '@prisma/client';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { NextPageWithLayout } from '../_app';
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPageWithLayout = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WriteResponse>('/api/posts');
+
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    post(data);
+  };
+
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
   return (
-    <form className="p-4 space-y-4">
-      <TextArea required placeholder="Ask a question!" />
-      <Button text="Submit" />
+    <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
+      <TextArea
+        register={register('question', { required: true, minLength: 5 })}
+        required
+        placeholder="Ask a question!"
+      />
+      <Button text={loading ? 'Loading' : 'Submit'} />
     </form>
   );
 };
