4d6ce1060ad79d2594f8ece6b31e6b9d6538bb57
김덕현 <oraclian@estsecurity.com>
2022-02-25 15:27:28 +0900

    build bug fix

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,12 +1,12 @@
-import { NextPageWithLayout } from '../_app';
 import Item from '@components/item';
 import Layout from '@components/layout';
+import { NextPageWithLayout } from '../_app';
 
 const Sold: NextPageWithLayout = () => {
   return (
     <div className="flex flex-col space-y-5 pb-10  divide-y">
       {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <Item id={i} key={i} title="iPhone 14" price={99} comments={1} hearts={1} />
+        <Item id={i} key={i} title="iPhone 14" price={99} hearts={1} />
       ))}
     </div>
   );
