4085483f31fc1343f3f28f722f80cbe88fcf89e1
김덕현 <oraclian@estsecurity.com>
2022-03-02 09:01:36 +0900

    Reviews

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,7 +4,7 @@ declare global {
   var client: any;
 }
 
-const client = global.client || new PrismaClient();
+const client: PrismaClient = global.client || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') global.client = client;
 
