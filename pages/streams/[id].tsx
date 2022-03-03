e7deb3a096e890361987a82d165ef91da36824c5
김덕현 <oraclian@estsecurity.com>
2022-03-03 10:58:03 +0900

    Send Message

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -4,17 +4,33 @@ import { NextPageWithLayout } from '../_app';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { Stream } from '@prisma/client';
+import { useForm } from 'react-hook-form';
+import useMutation from '@libs/client/useMutation';
 
 interface StreamResponse {
   ok: true;
   stream: Stream;
 }
 
+interface MessageForm {
+  message: string;
+}
+
 const Stream: NextPageWithLayout = () => {
   const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data } = useSWR<StreamResponse>(
     router.query.id ? `/api/streams/${router.query.id}` : null,
   );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`,
+  );
+
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
+    reset();
+    sendMessage(form);
+  };
 
   return (
     <div className="space-y-4 py-10  px-4">
@@ -32,18 +48,21 @@ const Stream: NextPageWithLayout = () => {
           <Message message="미쳤어" />
         </div>
         <div className="fixed inset-x-0 bottom-0  bg-white py-2">
-          <div className="relative mx-auto flex w-full  max-w-md items-center">
+          <form
+            className="relative mx-auto flex w-full  max-w-md items-center"
+            onSubmit={handleSubmit(onValid)}
+          >
             <input
               type="text"
-              className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500
-              "
+              className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+              {...register('message', { required: true })}
             />
             <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
               <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
                 &rarr;
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
